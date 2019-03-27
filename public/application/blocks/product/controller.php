c77dba0f8f74ca30d0ed7a7d567d729dde3f6043
Danut Hostiuc <danut.hostiuc@5psolutions.eu>
2019-03-27 15:10:41 +0200

    - final

--- public/application/blocks/product/controller.php
+++ public/application/blocks/product/controller.php
@@ -3,6 +3,7 @@
 namespace Application\Block\Product;
 
 use Concrete\Core\Block\BlockController;
+use Concrete\Core\Legacy\Loader;
 use Doctrine\DBAL\Query\QueryBuilder;
 
 class Controller extends BlockController
@@ -72,35 +73,36 @@ class Controller extends BlockController
             $image_file_type = strtolower(pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION));
             $extensions_arr = array("png", "gif", "jpeg", "jpg");
             if (in_array($image_file_type, $extensions_arr)) {
-//                $conn = Loader::db();
-//                $sql = "INSERT INTO btProducts(bID, btTitle, btDescription, btPrice, btImage) VALUES (?, ?, ?, ?, ?)";
-//                $args = array(
-//                    $this->bID,
-//                    $this->post('title'),
-//                    $this->post('description'),
-//                    $this->post('price'),
-//                    $unique . "." . $image_file_type
-//                );
-//                $conn->Execute($sql, $args);
-                $conn = \Database::connection();
-                /** @var QueryBuilder $qb */
-                $qb = $conn->createQueryBuilder();
-                $qb->insert('btProducts')
-                    ->values(
-                        array(
-                            /*'bID' => '?',*/
-                            'btTitle' => '?',
-                            'btDescription' => '?',
-                            'btPrice' => '?',
-                            'btImage' => '?'
-                        )
-                    )
-                    /*->setParameter(0, $this->bID)*/
-                    ->setParameter(1, $this->post('title'))
-                    ->setParameter(2, $this->post('description'))
-                    ->setParameter(3, $this->post('price'))
-                    ->setParameter(4, $unique . '.' . $image_file_type)
-                    ->execute();
+                $conn = Loader::db();
+                $sql = "INSERT INTO btProducts(bID, btTitle, btDescription, btPrice, btImage) VALUES (?, ?, ?, ?, ?)";
+                $args = array(
+                    $this->bID,
+                    $this->post('title'),
+                    $this->post('description'),
+                    $this->post('price'),
+                    $unique . "." . $image_file_type
+                );
+                $conn->Execute($sql, $args);
+
+//                $conn = \Database::connection();
+//                /** @var QueryBuilder $qb */
+//                $qb = $conn->createQueryBuilder();
+//                $qb->insert('btProducts')
+//                    ->values(
+//                        array(
+//                            /*'bID' => '?',*/
+//                            'btTitle' => '?',
+//                            'btDescription' => '?',
+//                            'btPrice' => '?',
+//                            'btImage' => '?'
+//                        )
+//                    )
+//                    /*->setParameter(0, $this->bID)*/
+//                    ->setParameter(1, $this->post('title'))
+//                    ->setParameter(2, $this->post('description'))
+//                    ->setParameter(3, $this->post('price'))
+//                    ->setParameter(4, $unique . '.' . $image_file_type)
+//                    ->execute();
 
                 if (file_exists($target_dir . $name)) {
                     rename($target_dir . $name, $target_dir . $unique . "." . $image_file_type);
@@ -111,26 +113,27 @@ class Controller extends BlockController
             }
 
             if (!isset($_FILES["image"]["name"]) && empty($_FILES["image"]["name"])) {
-//                $conn = Loader::db();
-//                $sql = "INSERT INTO btProducts(bID, btTitle, btDescription, btPrice) VALUES (?, ?, ?, ?)";
-//                $args = array(
-//                    $this->bID,
-//                    $this->post('title'),
-//                    $this->post('description'),
-//                    $this->post('price'),
-//                );
-//                $conn->Execute($sql, $args);
-                $conn = \Database::connection();
-                /** @var QueryBuilder $qb */
-                $qb = $conn->createQueryBuilder();
-                $qb->update('btProducts')
-                    ->set('btTitle', '?')
-                    ->set('btDescription', '?')
-                    ->set('btPrice', '?')
-                    ->setParameter(0, $this->post('title'))
-                    ->setParameter(1, $this->post('description'))
-                    ->setParameter(2, $this->post('price'))
-                    ->execute();
+                $conn = Loader::db();
+                $sql = "INSERT INTO btProducts(bID, btTitle, btDescription, btPrice) VALUES (?, ?, ?, ?)";
+                $args = array(
+                    $this->bID,
+                    $this->post('title'),
+                    $this->post('description'),
+                    $this->post('price'),
+                );
+                $conn->Execute($sql, $args);
+
+//                $conn = \Database::connection();
+//                /** @var QueryBuilder $qb */
+//                $qb = $conn->createQueryBuilder();
+//                $qb->update('btProducts')
+//                    ->set('btTitle', '?')
+//                    ->set('btDescription', '?')
+//                    ->set('btPrice', '?')
+//                    ->setParameter(0, $this->post('title'))
+//                    ->setParameter(1, $this->post('description'))
+//                    ->setParameter(2, $this->post('price'))
+//                    ->execute();
             }
         }
     }
